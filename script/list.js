e117eaa2e2ec3f62d1f11c5c5facde877a7e646c
Titus Wormer <tituswormer@gmail.com>
2018-07-17 17:54:53 -0400

    Add multiple spaces in support list

--- script/list.js
+++ script/list.js
@@ -10,7 +10,8 @@ var svg = require('../lib/svg')
 var html = require('../lib/html')
 var merge = require('../lib/util/merge')
 
-var all = merge([xml, xlink, xmlns, svg, html, aria])
+var schemas = [xml, xlink, xmlns, svg, html, aria]
+var all = merge(schemas)
 
 module.exports = parseErrors
 
@@ -24,7 +25,7 @@ function transform(tree) {
 
 function visit(start, nodes, end) {
   var properties = all.property
-  var props = Object.keys(properties)
+  var props = Object.keys(properties).sort()
   var rows = [
     u('tableRow', [
       u('tableCell', [u('text', 'Property')]),
@@ -37,15 +38,29 @@ function visit(start, nodes, end) {
 
   function map(property) {
     var info = properties[property]
+    var spaces = []
     var fields = [
       u('tableCell', [u('inlineCode', property)]),
       u('tableCell', [u('inlineCode', info.attribute)])
     ]
 
-    if (info.space) {
-      fields.push(u('tableCell', [u('inlineCode', info.space)]))
+    schemas.forEach(exists)
+
+    if (spaces.length !== 0) {
+      fields.push(u('tableCell', spaces))
     }
 
     return u('tableRow', fields)
+
+    function exists(schema) {
+      var info = schema.property[property]
+      if (info && info.space) {
+        if (spaces.length !== 0) {
+          spaces.push(u('text', ', '))
+        }
+
+        spaces.push(u('inlineCode', info.space))
+      }
+    }
   }
 }
