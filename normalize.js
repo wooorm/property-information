496199ed23c1f980c8cb305657834d5619a6e64a
starptech <deusdustin@gmail.com>
2018-10-11 21:46:51 +0200

    [ci skip] lowercase

--- normalize.js
+++ normalize.js
@@ -6,7 +6,7 @@ function normalize(value) {
   const valueLowerCased = value.toLowerCase()
   const normalized = valueLowerCased.replace(/\b[:-]\b/g, '')
   // We preserve the attribute name
-  // When the attribute delimiter '-' was removed
+  // when the attribute delimiter '-' was removed
   if (normalized !== valueLowerCased) {
     return valueLowerCased
   }
