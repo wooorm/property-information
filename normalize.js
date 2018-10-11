1ad7f719972744c80bc49a2a4af69a6ea13d1a00
starptech <deusdustin@gmail.com>
2018-10-11 23:49:38 +0200

    try simplifying normalization

--- normalize.js
+++ normalize.js
@@ -3,12 +3,5 @@
 module.exports = normalize
 
 function normalize(value) {
-  const valueLowerCased = value.toLowerCase()
-  const normalized = valueLowerCased.replace(/\b[:-]\b/g, '')
-  // We preserve the attribute name
-  // when the attribute delimiter '-' was removed
-  if (normalized !== valueLowerCased) {
-    return valueLowerCased
-  }
-  return normalized
+  return value.toLowerCase()
 }
