1c30d29f58c31bca82a11319338553dab06fa6ef
starptech <deusdustin@gmail.com>
2018-10-11 21:46:08 +0200

    fix

--- normalize.js
+++ normalize.js
@@ -3,5 +3,12 @@
 module.exports = normalize
 
 function normalize(value) {
-  return value.toLowerCase().replace(/\b[:-]\b/g, '')
+  const valueLowerCased = value.toLowerCase()
+  const normalized = valueLowerCased.replace(/\b[:-]\b/g, '')
+  // We preserve the attribute name
+  // When the attribute delimiter '-' was removed
+  if (normalized !== valueLowerCased) {
+    return valueLowerCased
+  }
+  return normalized
 }
