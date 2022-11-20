7347fcb5ad602860d97531c0a2437bcabe193aa9
Titus Wormer <tituswormer@gmail.com>
2022-11-20 13:21:33 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -2,8 +2,8 @@
  * @typedef {import('./lib/util/schema.js').Schema} Schema
  */
 
-import assert from 'node:assert'
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {htmlElementAttributes} from 'html-element-attributes'
 import {svgElementAttributes} from 'svg-element-attributes'
 import {htmlEventAttributes} from 'html-event-attributes'
@@ -143,130 +143,126 @@ const nonStandardSVGAttributes = new Set([
   'pitch'
 ])
 
-test('schema', function (t) {
-  t.deepLooseEqual(information.html.property.className, {
+test('schema', function () {
+  assert.deepEqual(structuredClone(information.html.property.className), {
     space: 'html',
     attribute: 'class',
     property: 'className',
     spaceSeparated: true
   })
 
-  t.deepLooseEqual(information.html.property.srcSet, {
+  assert.deepEqual(structuredClone(information.html.property.srcSet), {
     space: 'html',
     attribute: 'srcset',
     property: 'srcSet'
   })
 
-  t.deepLooseEqual(information.html.property.download, {
+  assert.deepEqual(structuredClone(information.html.property.download), {
     space: 'html',
     attribute: 'download',
     property: 'download',
     overloadedBoolean: true
   })
 
-  t.deepLooseEqual(information.html.property.xmlLang, {
+  assert.deepEqual(structuredClone(information.html.property.xmlLang), {
     space: 'xml',
     attribute: 'xml:lang',
     property: 'xmlLang'
   })
 
-  t.deepLooseEqual(information.html.property.span, {
+  assert.deepEqual(structuredClone(information.html.property.span), {
     space: 'html',
     attribute: 'span',
     property: 'span',
     number: true
   })
 
-  t.deepLooseEqual(information.html.property.value, {
+  assert.deepEqual(structuredClone(information.html.property.value), {
     space: 'html',
     attribute: 'value',
     property: 'value',
     booleanish: true
   })
 
-  t.deepLooseEqual(information.html.property.itemScope, {
+  assert.deepEqual(structuredClone(information.html.property.itemScope), {
     space: 'html',
     attribute: 'itemscope',
     property: 'itemScope',
     boolean: true
   })
-
-  t.end()
 })
 
-test('normalize', function (t) {
-  t.equal(
+test('normalize', function () {
+  assert.equal(
     information.html.normal[normalize('className')],
     information.html.normal[normalize('class')],
     'properties should match normalized (#1)'
   )
 
-  t.equal(
+  assert.equal(
     information.svg.normal[normalize('xmlLang')],
     information.svg.normal[normalize('xml:lang')],
     'properties should match attributes (#2)'
   )
 
-  t.equal(
+  assert.equal(
     information.svg.normal[normalize('xLinkArcRole')],
     information.svg.normal[normalize('xlink:arcrole')],
     'properties should match attributes (#3)'
   )
 
-  t.equal(
+  assert.equal(
     information.html.normal[normalize('httpEquiv')],
     information.html.normal[normalize('http-equiv')],
     'properties should match attributes (#4)'
   )
 
-  t.equal(
+  assert.equal(
     information.html.normal[normalize('ariaValueNow')],
     information.html.normal[normalize('aria-valuenow')],
     'properties should match attributes (#5)'
   )
 
-  t.equal(
+  assert.equal(
     information.svg.normal[normalize('glyphOrientationVertical')],
     information.svg.normal[normalize('glyph-orientation-vertical')],
     'properties should match attributes (#6)'
   )
 
-  t.equal(
+  assert.equal(
     information.svg.normal[normalize('panose1')],
     information.svg.normal[normalize('panose-1')],
     'properties should match attributes (#7)'
   )
 
-  t.equal(
+  assert.equal(
     normalize(':class'),
     ':class',
     'attribute delimiters should remain if not following a word boundary (GH-7)'
   )
 
-  t.equal(
+  assert.equal(
     normalize('class-'),
     'class-',
     'attribute delimiters should remain if not preceding a word boundary (GH-7)'
   )
 
-  t.equal(
+  assert.equal(
     normalize('class-name'),
     'class-name',
     'attribute delimiters should remain otherwise it will be handled as a different known property called className (GH-12)'
   )
 
-  t.equal(
+  assert.equal(
     normalize('[cl]a[ss]'),
     '[cl]a[ss]',
     'non-attribute characters should not be removed (GH-7)'
   )
-
-  t.end()
 })
 
-test('find', function (t) {
-  t.deepLooseEqual(
-    find(information.html, 'for'),
+test('find', async function (t) {
+  assert.deepEqual(
+    structuredClone(find(information.html, 'for')),
     {
       space: 'html',
       attribute: 'for',
@@ -276,8 +272,8 @@ test('find', function (t) {
     'should find knowns by attribute'
   )
 
-  t.deepLooseEqual(
-    find(information.html, 'htmlFor'),
+  assert.deepEqual(
+    structuredClone(find(information.html, 'htmlFor')),
     {
       space: 'html',
       attribute: 'for',
@@ -287,8 +283,8 @@ test('find', function (t) {
     'should find knowns by property'
   )
 
-  t.deepLooseEqual(
-    find(information.html, 'FoR'),
+  assert.deepEqual(
+    structuredClone(find(information.html, 'FoR')),
     {
       space: 'html',
       attribute: 'for',
@@ -298,8 +294,8 @@ test('find', function (t) {
     'should find knowns by weirdly cased attribute'
   )
 
-  t.deepLooseEqual(
-    find(information.html, 'hTMLfOR'),
+  assert.deepEqual(
+    structuredClone(find(information.html, 'hTMLfOR')),
     {
       space: 'html',
       attribute: 'for',
@@ -309,61 +305,61 @@ test('find', function (t) {
     'should find knowns by weirdly cased property'
   )
 
-  t.deepLooseEqual(
-    find(information.html, 'xml:lang'),
+  assert.deepEqual(
+    structuredClone(find(information.html, 'xml:lang')),
     {space: 'xml', attribute: 'xml:lang', property: 'xmlLang'},
     'should find XML knowns by attribute'
   )
 
-  t.deepLooseEqual(
-    find(information.html, 'xmlLang'),
+  assert.deepEqual(
+    structuredClone(find(information.html, 'xmlLang')),
     {space: 'xml', attribute: 'xml:lang', property: 'xmlLang'},
     'should find knowns by property'
   )
 
-  t.deepLooseEqual(
-    find(information.html, 'xlink:arcrole'),
+  assert.deepEqual(
+    structuredClone(find(information.html, 'xlink:arcrole')),
     {space: 'xlink', attribute: 'xlink:arcrole', property: 'xLinkArcRole'},
     'should find XLink knowns by attribute'
   )
 
-  t.deepLooseEqual(
-    find(information.html, 'xLinkArcRole'),
+  assert.deepEqual(
+    structuredClone(find(information.html, 'xLinkArcRole')),
     {space: 'xlink', attribute: 'xlink:arcrole', property: 'xLinkArcRole'},
     'should find XLink knowns by property'
   )
 
-  t.deepLooseEqual(
-    find(information.html, 'xmlns:xlink'),
+  assert.deepEqual(
+    structuredClone(find(information.html, 'xmlns:xlink')),
     {space: 'xmlns', attribute: 'xmlns:xlink', property: 'xmlnsXLink'},
     'should find XMLNS knowns by attribute'
   )
 
-  t.deepLooseEqual(
-    find(information.html, 'xmlnsXLink'),
+  assert.deepEqual(
+    structuredClone(find(information.html, 'xmlnsXLink')),
     {space: 'xmlns', attribute: 'xmlns:xlink', property: 'xmlnsXLink'},
     'should find XMLNS knowns by property'
   )
 
-  t.deepLooseEqual(
-    find(information.html, 'aria-valuenow'),
+  assert.deepEqual(
+    structuredClone(find(information.html, 'aria-valuenow')),
     {attribute: 'aria-valuenow', property: 'ariaValueNow', number: true},
     'should find aria attributes'
   )
 
-  t.deepLooseEqual(
-    find(information.html, 'ariaValueNow'),
+  assert.deepEqual(
+    structuredClone(find(information.html, 'ariaValueNow')),
     {attribute: 'aria-valuenow', property: 'ariaValueNow', number: true},
     'should find aria properties'
   )
 
-  t.deepLooseEqual(
-    find(information.html, 'class-name'),
+  assert.deepEqual(
+    structuredClone(find(information.html, 'class-name')),
     {attribute: 'class-name', property: 'class-name'},
     'should not handle class-name as class property (GH-12)'
   )
 
-  t.test('data', function (st) {
+  await t.test('data', function () {
     const mapping = {
       'data-alpha': 'dataAlpha',
       'data-bravo-charlie': 'dataBravoCharlie',
@@ -387,85 +383,81 @@ test('find', function (t) {
         const property = mapping[attribute]
         index++
 
-        st.deepLooseEqual(
-          find(information.html, attribute),
+        assert.deepEqual(
+          structuredClone(find(information.html, attribute)),
           {attribute, property},
           'should find data (#' + index + ', attribute)'
         )
 
-        st.deepLooseEqual(
-          find(information.html, property),
+        assert.deepEqual(
+          structuredClone(find(information.html, property)),
           {attribute, property},
           'should find data (#' + index + ', property)'
         )
       }
     }
 
-    st.deepLooseEqual(
-      find(information.html, 'dataFoo-bar'),
+    assert.deepEqual(
+      structuredClone(find(information.html, 'dataFoo-bar')),
       {attribute: 'dataFoo-bar', property: 'dataFoo-bar'},
       'should ignore invalid properties'
     )
 
-    st.deepLooseEqual(
-      find(information.html, 'data!Foo-bar'),
+    assert.deepEqual(
+      structuredClone(find(information.html, 'data!Foo-bar')),
       {attribute: 'data!Foo-bar', property: 'data!Foo-bar'},
       'should ignore invalid attributes'
     )
-
-    st.end()
   })
 
-  t.deepLooseEqual(
-    find(information.html, 'foo'),
+  assert.deepEqual(
+    structuredClone(find(information.html, 'foo')),
     {attribute: 'foo', property: 'foo'},
     'should find unknown values (#1)'
   )
 
-  t.deepLooseEqual(
-    find(information.html, 'Bar'),
+  assert.deepEqual(
+    structuredClone(find(information.html, 'Bar')),
     {attribute: 'Bar', property: 'Bar'},
     'should find unknown values (#2)'
   )
 
-  t.deepLooseEqual(
-    find(information.html, 'BAZ'),
+  assert.deepEqual(
+    structuredClone(find(information.html, 'BAZ')),
     {attribute: 'BAZ', property: 'BAZ'},
     'should find unknown values (#3)'
   )
 
-  t.deepLooseEqual(
-    find(information.html, 'QuX'),
+  assert.deepEqual(
+    structuredClone(find(information.html, 'QuX')),
     {attribute: 'QuX', property: 'QuX'},
     'should find unknown values (#4)'
   )
 
-  t.equal(
+  assert.equal(
     find(information.html, 'id').defined,
     true,
     'should mark known properties as defined'
   )
 
-  t.equal(
+  assert.equal(
     find(information.html, 'data-x').defined,
     true,
     'should mark data properties as defined'
   )
 
-  t.equal(
+  assert.equal(
     find(information.html, 'foo').defined,
     false,
     'should mark undefined properties'
   )
-
-  t.end()
 })
 
-test('html', function (t) {
+test('html', function () {
   // Does this throw an error?
   // Then an attribute was likely recently added to HTML.
   // The solution is probably to define it in `lib/html.js`.
-  t.doesNotThrow(function () {
+  assert.doesNotThrow(function () {
     let index = -1
     while (++index < htmlAttributes.length) {
       assert(
@@ -475,7 +467,7 @@ test('html', function (t) {
     }
   }, 'known HTML attributes should be defined')
 
-  t.doesNotThrow(function () {
+  assert.doesNotThrow(function () {
     const info = Object.keys(information.html.property)
       .map((prop) => information.html.property[prop])
       .filter((info) => info.space === 'html')
@@ -489,12 +481,10 @@ test('html', function (t) {
       )
     }
   }, 'Defined HTML attributes should be known')
-
-  t.end()
 })
 
-test('svg', function (t) {
-  t.doesNotThrow(function () {
+test('svg', function () {
+  assert.doesNotThrow(function () {
     // Ignore these. In tiny they’re cased. In SVG2 they’re lowercase.
     const ignore = new Set(['playbackOrder', 'timelineBegin'])
     let index = -1
@@ -509,7 +499,7 @@ test('svg', function (t) {
     }
   }, 'known SVG attributes should be defined')
 
-  t.doesNotThrow(function () {
+  assert.doesNotThrow(function () {
     const info = Object.keys(information.svg.property)
       .map((prop) => information.svg.property[prop])
       .filter((info) => info.space === 'svg')
@@ -522,17 +512,15 @@ test('svg', function (t) {
       assert(defined, info[index].attribute + ' is not known')
     }
   }, 'Defined SVG attributes should be known')
-
-  t.end()
 })
 
-test('react', function (t) {
+test('react', function () {
   /** @type {string} */
   let type
 
   for (type in reactData) {
     if (own.call(reactData, type)) {
-      t.doesNotThrow(function () {
+      assert.doesNotThrow(function () {
         /** @type {Record<string, string>} */
         const data = reactData[type]
         /** @type {string} */
@@ -549,6 +537,4 @@ test('react', function (t) {
       }, 'known ' + type + ' properties should be defined')
     }
   }
-
-  t.end()
 })
